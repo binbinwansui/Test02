732823bd02deaf975b952746244f71e00fba67ae
Administrator <984641901@qq.com>
2019-09-10 21:10:16 +0800

    LALALA

---
+++ src/Hello.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author bin
+ * @create 2019 -09 -10 -17:03
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        List<String> list=new ArrayList<String>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+
+        if (list == null) {
+            
+        }
+        String str="haha";
+        if (list != null) {
+            System.out.println(str);
+        }
+    }
+
+
+
+}
